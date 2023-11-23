9b79ae794357ccae024bb2ba2c39b2d3c6cd68f9
Natalia Tilinina <natalia.tilinina@gmail.com>
2023-11-23 09:23:37 +0100

    3:usage message

--- script.py
+++ script.py
@@ -22,9 +22,34 @@ def parse_command_line():
 def fill_lists_irrational(xval, yval, function_number):
     if function_number == 1:
         yval.extend(xval)  # y = f(x) = x
+    elif function_number == 2:
+        yval.extend(x**2 for x in xval)  # y = f(x) = x**2
+    elif function_number == 3:
+        yval.extend(x**3 for x in xval)  # y = f(x) = x**3
+    elif function_number == 4:
+        yval.extend(math.sin(x) for x in xval)  # y = f(x) = sin(x)
+    elif function_number == 5:
+        yval.extend(math.cos(x) for x in xval)  # y = f(x) = cos(x)
+    elif function_number == 6:
+        yval.extend(math.tan(x) for x in xval)  # y = f(x) = tan(x)
     else:
         print("Function not implemented.")
+        print_usage()
         sys.exit(1)
+
+# Stage 6: Development Branch
+
+def print_usage():
+    print("Usage: python script.py <function_number>")
+    print("Supported functions:")
+    print("  1 - f(x) = x")
+    print("  2 - f(x) = x^2 (polynomial)")
+    print("  3 - f(x) = x^3 (polynomial)")
+    print("  4 - f(x) = sin(x) (trigonometric)")
+    print("  5 - f(x) = cos(x) (trigonometric)")
+    print("  6 - f(x) = tan(x) (trigonometric)")
+    print("  7 - f(x) = exp(x) (irrational)")
+    print("  8 - f(x) = sqrt(|x|) (irrational)")
 
 
 if __name__ == "__main__":
@@ -32,7 +57,7 @@ if __name__ == "__main__":
     print(f"Selected function number: {function_number}")
 
     # Define x values
-    xval = np.arange(-3.0, 3.0, 0.1).tolist()
+    xval = np.arange(-5.0, 5.0, 0.1).tolist()
 
     # Initialize y values
     yval = []
